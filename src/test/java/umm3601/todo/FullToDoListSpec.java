57f38750c51275124989a2f8636f9faf6022dc6a
Ai Sano <sanox013@morris.umn.edu>
2017-02-01 17:20:42 -0600

    Add test for correct first to-do

--- src/test/java/umm3601/todo/FullToDoListSpec.java
+++ src/test/java/umm3601/todo/FullToDoListSpec.java
@@ -15,4 +15,16 @@ public class FullToDoListSpec {
         ToDo[] allToDos = toDoController.listToDos(new HashMap<>());
         assertEquals("Incorrect total number of toDos",300, allToDos.length);
     }
+
+    @Test
+    public void firstTodoInFullList() throws IOException {
+        ToDoController toDoController = new ToDoController();
+        ToDo[] allTodos = toDoController.listToDos(new HashMap<>());
+        ToDo firstTodo = allTodos[0];
+        assertEquals("Incorrect owner", "Blanche", firstTodo.owner);
+        assertEquals("Incorrect status", false, firstTodo.status);
+        assertEquals("Incorrect category", "software design", firstTodo.category);
+    }
+
+
 }
