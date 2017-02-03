1cb9af3802d7f0d42508cf152a55924dad5a9416
Ai Sano <sanox013@morris.umn.edu>
2017-02-02 20:04:30 -0600

    Add ability in server to filter to-dos by category
    Also added a test for this feature
    
    Closes #18

--- src/test/java/umm3601/todo/FilterToDoListSpec.java
+++ src/test/java/umm3601/todo/FilterToDoListSpec.java
@@ -20,4 +20,13 @@ public class FilterToDoListSpec {
         assertEquals("Incorrect number of to-dos with owner Blanche", 43, ownerBlanche.length);
     }
 
+    @Test
+    public void filterToDosByCategory() throws IOException {
+        ToDoController toDoController = new ToDoController();
+        ToDo[] allToDos = toDoController.listToDos(new HashMap<>());
+        ToDo[] categoryHomework = toDoController.filterToDosByCategory(allToDos, "homework");
+        assertEquals("Incorrect number of to-dos with category homework", 79, categoryHomework.length);
+        ToDo[] categoryVideoGames = toDoController.filterToDosByCategory(allToDos, "video games");
+        assertEquals("Incorrect number of to-dos with category video games", 71, categoryVideoGames.length);
+    }
 }
